8d77b28fd4547b9c85d4225b8dbb1b5475c54a84
Joshua Tan <joshuatanir@gmail.com>
2026-06-12 12:14:05 +0800

    c00: alphabets a-z in lower case

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void);
+
+void ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz",26);
+}
+
+int main (void)
+{
+	ft_print_alphabet();
+
+	return 0;
+}
