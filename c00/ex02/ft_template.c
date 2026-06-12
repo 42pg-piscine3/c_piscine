b0d7587c4fec1e6135778876e87e29d9c99e23f3
Joshua Tan <joshuatanir@gmail.com>
2026-06-12 12:46:38 +0800

    c00: print alphabet z..a

---
+++ c00/ex02/ft_template.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+//function prototype
+// e.g. void ft_<something> (<type> name);
+void ft_print_reverse_alphabet (void);
+
+//function definition
+void ft_print_reverse_alphabet (void)
+{
+	write(1,"",26);
+
+
+
+int main (void)
+{
+
+	// function call
+	
+	return 0;
+}
