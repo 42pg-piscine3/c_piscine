640d958d6effefe3e9d0a5295615d7260d09c722
Joshua Tan <joshuatanir@gmail.com>
2026-06-14 05:47:13 +0800

    rush00: Rush01-change function names and add comments

--- rush00/rush01.c
+++ rush00/rush01.c
@@ -6,16 +6,34 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:08:08 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/14 03:54:56 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/14 05:44:29 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 void	ft_putchar(char draw);
 
-void	draw_firstrow (int col)
+/*
+ *	This function draws column by column when the program is at first row (top)
+ *
+ *	col_count is used to indicate which column the program is on.
+ *	A loop is used to
+ *	1. increment the col_count (from 1 to the "col")
+ *	2. decide what to draw depending on col_count.
+ *
+ *		1 <columns between 1 and the last> <last>
+ *
+ *		| ||||||||||||||||||||||||||||||| |
+ *		v vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv v
+ *
+ *		/ ******************************* \\ 
+ *
+ *	At the end of the column, place a newline (\n) character.
+ */
+void	drawrow_first(int col)
 {
-	int col_count;
+	int	col_count;
+
 	col_count = 1;
 	while (col_count <= col)
 	{
@@ -33,13 +51,31 @@ void	draw_firstrow (int col)
 		}
 		col_count++;
 	}
-
 	ft_putchar('\n'); //put newline at the end column
 }
 
-void	draw_middlerows (int col)
+/*
+ *	This function draws column by column for row the program is at.
+ *
+ *	col_count is used to indicate which column the program is on.
+ *	A loop is used to
+ *	1. increment the col_count (from 1 to the "col")
+ *	2. decide what to draw depending on col_count.
+ *
+ *		1 <columns between 1 and the last> <last>
+ *
+ *		| ||||||||||||||||||||||||||||||| |
+ *		v vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv v
+ *
+ *		|                                 |
+ *		|                                 |
+ *
+ *	At the end of the column, place a newline (\n) character.
+ */
+void	drawrow_middles(int col)
 {
-	int col_count;
+	int	col_count;
+
 	col_count = 1;
 	while (col_count <= col)
 	{
@@ -57,78 +93,90 @@ void	draw_middlerows (int col)
 		}
 		col_count++;
 	}
-
 	ft_putchar('\n'); //put newline at the end column
-
 }
 
-void	draw_lastrow (int col)
+/*
+ *	This function draws column by column when the program is at last row
+ *
+ *	col_count is used to indicate which column the program is on.
+ *	A loop is used to
+ *	1. increment the col_count (from 1 to the "col")
+ *	2. decide what to draw depending on col_count.
+ *
+ *		1 <columns between 1 and the last> <last>
+ *
+ *		| ||||||||||||||||||||||||||||||| |
+ *		v vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv v
+ *
+ *		\ ******************************* /
+ *
+ *	At the end of the column, place a newline (\n) character.
+ */
+void	drawrow_last(int col)
 {
+	int	col_count;
 
-	int col_count;
 	col_count = 1;
 	while (col_count <= col)
 	{
-		if (col_count == 1) //Start with 1 for natural language
+		if (col_count == 1)
 		{
 			ft_putchar('\\');
 		}
-		else if (col_count < col) //other columns than start or end
+		else if (col_count < col)
 		{
 			ft_putchar('*');
 		}
-		else // end of column
+		else
 		{
 			ft_putchar('/');
 		}
 		col_count++;
 	}
-
-	ft_putchar('\n'); //put newline at the end column
-
+	ft_putchar('\n');
 }
 
-
-// x = column, y = row
+/*
+ *	This function controls the row traversal.
+ *
+ *	row_count is used to indicate which row the program is on.
+ *	A loop is used to
+ *	1. increment the row_count (from 1 to the "row")
+ *	2. decide what to draw depending on row_count.
+ *
+ *	when row_count is the first	- call a drawing function for the first row
+ *  when row_count is not first/last - call a drawing function for middle rows
+ *  when row_count is the last - call a drawing function for the last row
+ *
+ *	note: row_count can only be incremented at the end of the loop.
+ *	If row_count is at the beginnin of the loop, the decisions will be wrong.
+ *
+ */
+//			x = column, y = row
 void	rush(int x, int y)
 {
 	int	row;
 	int	col;
-	int	col_count;
 	int	row_count;
 
 	row = y;
 	col = x;
 	row_count = 1; //Start row_count with 1 for natural language
-
-	// | row loop 
-	// V
 	while (row_count <= row)
-	{	
-		//printf("row_count = %d\n", row_count);
-		//col_count = 1; //Reset column count when starting a new row
-
-		//column loop -->>
-		//while (col_count <= col)
-		//{
-			//printf("col_count = %d\n", col_count);
-
-			if (row_count == 1)
-			{
-				draw_firstrow(col);
-			} 
-			else if (row_count < row)
-			{
-				draw_middlerows(col);	
-			}
-			else //end of row
-			{
-				draw_lastrow(col);				
-			}
-		//	col_count = col_count + 1; //go to next column
-		//}
-
-		////ft_putchar('\n'); //put newline at the end column
-		row_count = row_count + 1; //go to next row
+	{
+		if (row_count == 1)
+		{
+			drawrow_first(col);
+		}
+		else if (row_count < row)
+		{
+			drawrow_middles(col);
+		}
+		else
+		{
+			drawrow_last(col);
+		}
+		row_count = row_count + 1;
 	}
 }
