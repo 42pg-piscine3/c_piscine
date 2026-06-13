8fa315439e2306efa7e7747890bb49ad14640b81
Joshua Tan <joshuatanir@gmail.com>
2026-06-14 07:37:25 +0800

    rush00: Rush0X-[WIP] create array of patterns

--- rush00/main.c
+++ rush00/main.c
@@ -6,14 +6,24 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:06:21 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/14 06:46:06 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/14 07:34:13 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+int option_rush = 0;
+
 void	rush(int x, int y);
 
+
 // Rush function has the parameters: rush(columns, rows)
-int	main(void)
+int	main(int argc, char *argv)
 {
+	if (2 == argc)
+	{
+		printf("Option passed in: %c\n",argv[1]);
+		option_rush	= atoi(*argv[1]);
+	}
 	rush(5, 5);
 	rush(0, 0);
 	rush(5, 3);
