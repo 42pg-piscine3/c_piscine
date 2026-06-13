c7e18403d55c0c2b3e4eb11c317e66c34d57cf1e
Joshua Tan <joshuatanir@gmail.com>
2026-06-14 03:49:59 +0800

    rush00: Rush01-Make separate functions for drawing each row's sign
    rush functions is a contains a row control loop.
    For top, middle and last rows, different functions are called to
    draw the symbols accordingly

--- rush00/rush01.c
+++ rush00/rush01.c
@@ -6,12 +6,88 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:08:08 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/13 17:49:59 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/14 03:47:08 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 void	ft_putchar(char draw);
+
+void	draw_firstrow (int col)
+{
+	int col_count;
+	col_count = 1;
+	while (col_count <= col)
+	{
+		if (col_count == 1) //Start with 1 for natural language
+		{
+			ft_putchar('o');
+		}
+		else if (col_count < col) //other columns than start or end
+		{
+			ft_putchar('-');
+		}
+		else // end of column
+		{
+			ft_putchar('o');
+		}
+		col_count++;
+	}
+
+	ft_putchar('\n'); //put newline at the end column
+}
+
+void	draw_middlerows (int col)
+{
+	int col_count;
+	col_count = 1;
+	while (col_count <= col)
+	{
+		if (col_count == 1) //Start with 1 for natural language
+		{
+			ft_putchar('|');
+		}
+		else if (col_count < col) //other columns than start or end
+		{
+			ft_putchar(' ');
+		}
+		else // end of column
+		{
+			ft_putchar('|');
+		}
+		col_count++;
+	}
+
+	ft_putchar('\n'); //put newline at the end column
+
+}
+
+void	draw_lastrow (int col)
+{
+
+	int col_count;
+	col_count = 1;
+	while (col_count <= col)
+	{
+		if (col_count == 1) //Start with 1 for natural language
+		{
+			ft_putchar('o');
+		}
+		else if (col_count < col) //other columns than start or end
+		{
+			ft_putchar('-');
+		}
+		else // end of column
+		{
+			ft_putchar('o');
+		}
+		col_count++;
+	}
+
+	ft_putchar('\n'); //put newline at the end column
+
+}
+
 
 // x = column, y = row
 void	rush(int x, int y)
@@ -20,7 +96,7 @@ void	rush(int x, int y)
 	int	col;
 	int	col_count;
 	int	row_count;
-	
+
 	row = y;
 	col = x;
 	row_count = 1; //Start row_count with 1 for natural language
@@ -30,62 +106,29 @@ void	rush(int x, int y)
 	while (row_count <= row)
 	{	
 		//printf("row_count = %d\n", row_count);
-		col_count = 1; //Reset column count when starting a new row
-		
+		//col_count = 1; //Reset column count when starting a new row
+
 		//column loop -->>
-		while (col_count <= col)
-		{
+		//while (col_count <= col)
+		//{
 			//printf("col_count = %d\n", col_count);
 
 			if (row_count == 1)
 			{
-				if (col_count == 1) //Start with 1 for natural language
-				{
-					ft_putchar('o');
-				}
-				else if (col_count < col) //other columns than start or end
-				{
-					ft_putchar('-');
-				}
-				else // end of column
-				{
-					ft_putchar('o');
-				}	
+				draw_firstrow(col);
 			} 
 			else if (row_count < row)
 			{
-				if (col_count == 1) //Start with 1 for natural language
-				{
-					ft_putchar('|');
-				}
-				else if (col_count < col) //other columns than start or end
-				{
-					ft_putchar(' ');
-				}
-				else // end of column
-				{
-					ft_putchar('|');
-				}				
+				draw_middlerows(col);	
 			}
 			else //end of row
 			{
-				if (col_count == 1) //Start with 1 for natural language
-				{
-					ft_putchar('o');
-				}
-				else if (col_count < col) //other columns than start or end
-				{
-					ft_putchar('-');
-				}
-				else // end of column
-				{
-					ft_putchar('o');
-				}						
+				draw_lastrow(col);				
 			}
-			col_count = col_count + 1; //go to next column
-		}
+		//	col_count = col_count + 1; //go to next column
+		//}
 
-		ft_putchar('\n'); //put newline at the end column
+		////ft_putchar('\n'); //put newline at the end column
 		row_count = row_count + 1; //go to next row
 	}
 }
