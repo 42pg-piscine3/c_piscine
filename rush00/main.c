113f28e532f35268e891c3facc280f77509e045c
Joshua Tan <joshuatanir@gmail.com>
2026-06-14 07:03:10 +0800

    rush00: Rush01-Change printing of x,y to rush()

--- rush00/main.c
+++ rush00/main.c
@@ -6,36 +6,21 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:06:21 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/13 17:50:34 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/14 06:46:06 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h> // for using printf
-
 void	rush(int x, int y);
 
 // Rush function has the parameters: rush(columns, rows)
 int	main(void)
 {
-	printf(" rush (5,5)\n");
-	rush (5, 5);
-	
-	printf(" \nrush (5,3)\n");
+	rush(5, 5);
+	rush(0, 0);
 	rush(5, 3);
-	
-	printf (" \nrush (5,1)\n");
 	rush(5, 1);
-	
-	printf(" \nrush (1,1)\n");
 	rush(1, 1);
-	
-	printf(" \nrush (1,5)\n");
 	rush(1, 5);
-	
-	printf(" \nrush (4,4)\n");
 	rush(4, 4);
-	
-	printf(" \nrush (2,2)\n");
 	rush(2, 2);
-	
 	return (0);
 }
--- rush00/rush01.c
+++ rush00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 15:08:08 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/14 06:17:01 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/14 07:01:35 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -156,6 +156,7 @@ void	rush(int x, int y)
 	row = y;
 	col = x;
 	row_count = 1;
+	printf("rush(%d, %d):\n", x, y);
 	while (row_count <= row)
 	{
 		if (row_count == 1)
