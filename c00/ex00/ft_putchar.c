e8f1ffdd8f654607a9ab425baab63662027f3f6f
Joshua Tan <joshuatanir@gmail.com>
2026-06-19 10:44:08 +0800

    c00: ex01-Disable main, add 42 header

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 10:43:03 by joshtan           #+#    #+#             */
+/*   Updated: 2026/06/19 10:43:37 by joshtan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-	write(1,&c, 1);	
+	write(1, &c, 1);
 }
 
-
+/*
 int main (void)
 {
 	ft_putchar('a');
 
 	return 0;
 }
-
+*/
