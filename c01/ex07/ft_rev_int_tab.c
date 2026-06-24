0964b7a62d1efa510046d2e02d62ecef644e56af
Joshua Tan <joshuatanir@gmail.com>
2026-06-24 12:38:22 +0800

    c01: ex07-WIP

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,20 +6,25 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 10:53:09 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/24 11:09:20 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/24 12:37:56 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int track;
+	int element;
+	int tmp;
 
-	printf("*tab=%d, *(tab+(size-1)):%d\n", *tab, *(tab+(size-1)));
+	//printf("first @*tab=%d, last @*(tab+(size-1)):%d\n", *tab, *(tab+(size-1)));
 
-	while (track < size)
+	element = 0;
+	while (element < size)
 	{
-		*tab = *(tab);
+		tmp = *(tab+(size-1-element));
+		*(tab+(size-1-element)) = *tab;
+		*tab = tmp;
+		element++;
 	}
 }
 
@@ -28,7 +33,9 @@ void	ft_rev_int_tab(int *tab, int size)
  */
 int main (int argc, char *argv[])
 {
-	int	tab[] = {1,2,3,4,5,6,7,8,9,0};
+	int	tab[] = {1,2,3,4,5,6,7,8,9,0,123};
 
 	printf("sizeof tab= %ld \n", (sizeof(tab)/sizeof(int)));
+	ft_rev_int_tab(tab, sizeof(tab)/sizeof(int));
+	//printf("first @*tab=%d, last @*(tab+(size-1)):%d\n", *tab, *(tab+(size-1)));
 }/* */
