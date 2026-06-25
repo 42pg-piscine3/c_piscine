03f3395f0d239d3134d82d19cdf924ab4ef6d772
Joshua Tan <joshuatan.ir@gmail.com>
2026-06-25 15:43:50 +0800

    c01: ex03-merge main.c contents to solution

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -6,15 +6,18 @@
 /*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 01:34:34 by joshtan           #+#    #+#             */
-/*   Updated: 2026/06/23 03:08:23 by joshtan          ###   ########.fr       */
+/*   Updated: 2026/06/25 15:42:18 by joshtan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+#include <unistd.h>
+*/
 
 /*
  *  divides ‘a’ by ‘b’store result in the integer pointed to by ‘div’.
  *	stores the remainder of the division of in the integer pointed to by ‘mod’.
  */
-
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	if (b == 0)
@@ -28,3 +31,46 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 		*mod = a % b;
 	}
 }
+
+
+
+/*
+ * Uncomment to Test
+
+void    ft_putnbr(int n)
+{
+    char    c;
+
+    if (n < 0)
+    {
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr(n / 10);
+    n = n % 10;
+    c = n + '0';
+    write(1, &c, 1);
+}
+
+int main(void)
+{
+    int div;
+    int mod;
+    int a;
+    int b;
+
+    div = 0;
+    mod = 0;
+    ft_putnbr(div);
+    write(1, "\n", 1);
+    ft_putnbr(mod);
+    write(1, "\n", 1);
+    a = 56;
+    b = 5;
+    ft_div_mod(a, b, &div, &mod);
+    ft_putnbr(div);
+    write(1, "\n", 1);
+    ft_putnbr(mod);
+    write(1, "\n", 1);
+}
+*/
