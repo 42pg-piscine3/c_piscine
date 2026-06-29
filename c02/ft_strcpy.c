9742b28b036ae734cb747baa2263f682154ab40a
Joshua Tan <joshuatanir@gmail.com>
2026-06-30 02:10:07 +0800

    c02: ft_strcpy

---
+++ c02/ft_strcpy.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joshtan <joshtan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 21:14:10 by joshtan           #+#    #+#             */
+/*   Updated: 2026/06/30 02:01:41 by joshtan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Implement char *my_strcpy(char *dest, char *src);
+ * Returns pointer to dest string.
+ * Copying includes '\0'
+ * Function stop copying after null from src is written.
+ */
+/*
+ * Tricky bit:
+ * idiomatic way is while(*dest=*src)
+ * However Norminette flags assignment inside loop control.
+ * We cannot use while(*dest) because it could contain '\0' randomly, it may
+ * terminate prematurely.
+ *
+ * So while(*src) at least guarantees a null terminated string is detect right.
+ * However, when *src == '\0', the while loop ends and skips copying the null
+ * to *dest. *dest becomes non-null terminated. We have to assign null after
+ * loop exits.
+ *
+ * "safer" way is directly memcpy, but the exercise disallows other functions.
+ */
+char	*my_strcpy(char *dest, char *src)
+{
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
+
+/*
+ * Uncomment to Test.
+ **
+#include <stdio.h>
+#include <string.h>
+
+int	main(int argc, char *argv[])
+{
+	char	*orig;
+	char	*copy;
+	char	s[12];
+	int		idx;
+
+	idx = 0;
+	if (argc != 3)
+	{
+		my_strcpy(s, "abracadabra");
+		printf("%s\n", s);
+		while (idx < strlen(s))
+		{
+			if (s[idx] == 'a')
+				my_strcpy(s + idx, s + idx + 1);
+			else
+				idx++;
+		}
+		printf("%s\n", s);
+	}
+}
+**/
